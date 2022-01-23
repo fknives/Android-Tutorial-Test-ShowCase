4a1254d092656693ad5f8554abae70f59fab4fd2
Gergely Hegedus <hegedusgergely@yahoo.com>
2022-01-23 10:07:42 +0200

    Issue#3 Start exporting schemas

--- app/src/main/java/org/fnives/test/showcase/storage/LocalDatabase.kt
+++ app/src/main/java/org/fnives/test/showcase/storage/LocalDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import org.fnives.test.showcase.storage.favourite.FavouriteDao
 import org.fnives.test.showcase.storage.favourite.FavouriteEntity
 
-@Database(entities = [FavouriteEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [FavouriteEntity::class],
+    version = 1,
+    exportSchema = true
+)
 abstract class LocalDatabase : RoomDatabase() {
 
     abstract val favouriteDao: FavouriteDao
