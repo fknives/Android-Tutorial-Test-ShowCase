27f1276cc12d4576fdf929ca18ce7d78a5be3a4a
Gergely Hegedus <hegedusgergely@yahoo.com>
2022-05-17 15:10:29 +0300

    Issue#84 Ensure BackgroundThread work is synced for DiffUtil

---
+++ app/src/main/java/org/fnives/test/showcase/ui/shared/executor/AsyncTaskExecutor.kt
@@ -0,0 +1,28 @@
+package org.fnives.test.showcase.ui.shared.executor
+
+import java.util.concurrent.Executor
+
+/**
+ * Basic copy of [ArchTaskExecutor][androidx.arch.core.executor.ArchTaskExecutor], needed because that is restricted to Library.
+ *
+ * Intended to be used for [AsyncDifferConfig][androidx.recyclerview.widget.AsyncDifferConfig] so it can be synchronized with Espresso.
+ *
+ * Workaround until https://github.com/android/android-test/issues/382 is fixed finally.
+ */
+object AsyncTaskExecutor : TaskExecutor {
+
+    val mainThreadExecutor = Executor { command -> postToMainThread(command) }
+    val iOThreadExecutor = Executor { command -> executeOnDiskIO(command) }
+
+    var delegate: TaskExecutor? = null
+    private val defaultExecutor by lazy { DefaultTaskExecutor() }
+    private val executor get() = delegate ?: defaultExecutor
+
+    override fun executeOnDiskIO(runnable: Runnable) {
+        executor.executeOnDiskIO(runnable)
+    }
+
+    override fun postToMainThread(runnable: Runnable) {
+        executor.postToMainThread(runnable)
+    }
+}
---
+++ app/src/sharedTest/java/org/fnives/test/showcase/testutils/idling/AsyncDiffUtilInstantTestRule.kt
@@ -0,0 +1,32 @@
+package org.fnives.test.showcase.testutils.idling
+
+import org.fnives.test.showcase.ui.shared.executor.AsyncTaskExecutor
+import org.fnives.test.showcase.ui.shared.executor.TaskExecutor
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/**
+ * Similar Test Rule to InstantTaskExecutorRule just for the [AsyncTaskExecutor] to make AsyncDiffUtil synchronized.
+ */
+class AsyncDiffUtilInstantTestRule : TestRule {
+    override fun apply(base: Statement, description: Description): Statement =
+        object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                AsyncTaskExecutor.delegate = object : TaskExecutor {
+                    override fun executeOnDiskIO(runnable: Runnable) {
+                        runnable.run()
+                    }
+
+                    override fun postToMainThread(runnable: Runnable) {
+                        runnable.run()
+                    }
+                }
+
+                base.evaluate()
+
+                AsyncTaskExecutor.delegate = null
+            }
+        }
+}
