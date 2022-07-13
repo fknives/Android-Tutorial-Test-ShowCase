d23c1b9a9da0d448bc2c0fd54d81f59f6b4310b6
Gergely Hegedus <hegedusgergely@yahoo.com>
2022-07-13 19:13:41 +0300

    Issue#100 Fix screenshot rule for API 21
    On API 21 the images could not have been saved before.
    Added logic to choose external or internal folder.
    Tested on:
    API 21, 24, 30, 31, 32

--- test-util-android/src/main/java/org/fnives/test/showcase/android/testutil/screenshot/basicScreenCaptureProcessor.kt
+++ test-util-android/src/main/java/org/fnives/test/showcase/android/testutil/screenshot/basicScreenCaptureProcessor.kt
@@ -1,19 +1,40 @@
 package org.fnives.test.showcase.android.testutil.screenshot
 
-import android.os.Build
 import android.os.Environment
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.screenshot.ScreenCaptureProcessor
 import androidx.test.runner.screenshot.basicScreenCaptureProcessor
 import java.io.File
 
-fun basicScreenCaptureProcessor(subDir: String = "test-screenshots") =
-    basicScreenCaptureProcessor(File(getTestPicturesDir(), subDir))
+fun basicScreenCaptureProcessor(subDir: String = "test-screenshots"): ScreenCaptureProcessor {
+    val directory = File(getTestPicturesDir(), subDir)
+    Log.d(ScreenshotRule.TAG, "directory to save screenshots = $directory")
+    return basicScreenCaptureProcessor(File(getTestPicturesDir(), subDir))
+}
 
+/**
+ * BasicScreenCaptureProcessor seems to work differently on API versions,
+ * based on where we have access to save and pull the images from.
+ *
+ * see example issue: https://github.com/android/android-test/issues/818
+ */
 @Suppress("DEPRECATION")
-fun getTestPicturesDir() =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        InstrumentationRegistry.getInstrumentation().targetContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-    } else {
-        val packageName = InstrumentationRegistry.getInstrumentation().targetContext.packageName
-        File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), packageName)
+fun getTestPicturesDir(): File? {
+    val packageName = InstrumentationRegistry.getInstrumentation().targetContext.packageName
+    val environmentFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+    val externalFolder = File(environmentFolder, packageName)
+    if (externalFolder.canWrite()) {
+        Log.d(ScreenshotRule.TAG, "external folder")
+        return externalFolder
     }
+
+    val internalFolder = InstrumentationRegistry.getInstrumentation().targetContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    if (internalFolder?.canWrite() == true) {
+        Log.d(ScreenshotRule.TAG, "internal folder")
+        return internalFolder
+    }
+    Log.d(ScreenshotRule.TAG, "cant find directory the screenshots could be saved into")
+
+    return null
+}
